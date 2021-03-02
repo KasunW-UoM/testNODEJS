c039c103fdc6994ca01db3e11cd67f73ec2e7a58
Kasun madhulal weerasekara <73052192+KasunW-UoM@users.noreply.github.com>
2021-03-03 01:27:27 +0530

    Add files via upload

---
+++ circle.js
@@ -0,0 +1,30 @@
+/*
+find the diametar,area and circumferrnce of a circle of radius 20cm.
+diameter of circle D=2*r
+PI=3.141
+AREA of a circle A=PI*r*r
+circumferrnce of a circle C=2*PI*r
+*/
+
+let radius=20;
+let PI=3.141;
+let diameter;
+
+diameter=2*radius;
+
+let circumferrnce = 2*PI*radius;
+
+let area=PI*radius*radius;
+
+console.log("                                                  ");
+
+console.log("diameter = "+diameter);
+console.log("circumferrnce  = "+circumferrnce );
+console.log("area= "+area);
+console.log("===============================================");
+console.log("                                                  ");
+
+console.log("Kasun practise node.js code sloveing in 2021.03.03");
+console.log("===============================================");
+console.log("                                                  ");
+
---
+++ var.js
@@ -0,0 +1,12 @@
+let firstName = "Kasun";//string
+let a = 10; //integar
+let b =20;
+
+console.log(a+b);
+console.log(a%b);
+console.log(a-b);
+console.log(a/b);
+console.log(a*b);
+//ture or flase
+
+let x=true;
